815f49aa00f28f363b8191ebb3818f821a6eef22
Soubhik Karmakar <soubhikk04@gmail.com>
2026-05-16 19:38:58 +0000

    understanding variables

---
+++ 01_basics/variables_01.js
@@ -0,0 +1,9 @@
+const accountId = 145646 ;
+let accountEmail = "soubhik@gmail.com";
+var accountPassword = "123456";
+accountCity = "Kolkata";
+let accountState;
+
+// accountId = 2; not allowed const variable can not change
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
