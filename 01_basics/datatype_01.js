9ea85cc8ebcb64dfc1417fe58b77f6396c86d876
Soubhik Karmakar <soubhikk04@gmail.com>
2026-05-17 15:42:39 +0000

    DataTypes

---
+++ 01_basics/datatype_01.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let name = "soubhik";
+let age = 24;
+let isLoggedIn = true;
+let state;
+let temperature = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof temperature);
